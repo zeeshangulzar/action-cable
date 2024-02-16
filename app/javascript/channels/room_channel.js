2f4a5f238dd871291f72fb0a29b4cf08336b4092
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2024-02-16 17:50:06 +0500

    Action cable basics

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,16 +1,27 @@
 import consumer from "channels/consumer"
 
-consumer.subscriptions.create("RoomChannel", {
-  connected() {
-    console.log("Connected to room channel.......")
-    // Called when the subscription is ready for use on the server
-  },
+document.addEventListener('turbo:load', () => {
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+  const element = document.getElementById('room-id')
+  const room_id = element.getAttribute('data-room-id')
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
+  console.log(room_id)
+
+  consumer.subscriptions.create({channel: "RoomChannel",
+  room_id: room_id}, {
+    connected() {
+      console.log("Connected to room channel " + room_id)
+      // Called when the subscription is ready for use on the server
+    },
+  
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+  
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+     console.log(data)
+    }
+  });
+  
+})
